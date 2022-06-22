99dfed3d3ef301389c0687a50111f783559fc639
megis <atanmegis.cn@gmail.com>
2022-06-22 14:36:40 +0700

    finished

--- src/components/hooks/useHttp.js
+++ src/components/hooks/useHttp.js
@@ -1,39 +1,38 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback } from "react";
 
 const useHttp = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
-    const sendRequest = useCallback(async (requestConfig, applyData) => {
-        setIsLoading(true);
-        try {
-            const response = await fetch(requestConfig.url, {
-              method: requestConfig.method ? requestConfig.method : 'GET',
-              headers: requestConfig.headers ? requestConfig.headers : {},
-              body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-            });
-      
-            if (!response.ok) {
-              throw new Error('Request failed!');
-            }
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
 
-            const data = await response.json();
-            applyData(data);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-    }, []) ;
-
-    return {
-        isLoading: isLoading,
-        error: error,
-        sendRequest: sendRequest,
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
     }
-}
+    setIsLoading(false);
+  }, []);
 
-export default useHttp
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequest: sendRequest,
+  };
+};
+
+export default useHttp;
 
 /*import { useState, useCallback } from 'react';
 
